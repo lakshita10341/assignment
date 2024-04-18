7121e406e730f11e5e92a43ee7f457f3ede50854
lakshita10341 <lakshitajainchopra89@gmail.com>
2024-04-18 18:37:42 +0530

    review function update

--- code.cpp
+++ code.cpp
@@ -10,31 +10,31 @@ class IMG_Member
 protected:
    string name;
    string role;
- 
+
 public:
    IMG_Member(string name, string role) : name(name), role(role) {}
-   
-  virtual int give_choices()const =0;
-  
 
-  string get_current_date(){
-    time_t now = time(0);
-    tm *ltm = localtime(&now);
-     string date = to_string(1900 + ltm->tm_year) + "-" + to_string(1 + ltm->tm_mon) + "-" + to_string(ltm->tm_mday);
-     return date;
-  }
-     
- void view_assignment_status(string &s_name)
+   virtual int give_choices() const = 0;
+
+   string get_current_date()
+   {
+      time_t now = time(0);
+      tm *ltm = localtime(&now);
+      string date = to_string(1900 + ltm->tm_year) + "-" + to_string(1 + ltm->tm_mon) + "-" + to_string(ltm->tm_mday);
+      return date;
+   }
+
+   void view_assignment_status(string &s_name)
    {
       string line;
       string filename = s_name + ".txt";
       ifstream file(filename);
       if (file.is_open())
       {
-         int count=0;
+         int count = 0;
          while (getline(file, line))
          {
-            if(count>3)
+            if (count > 3)
                cout << line << endl;
             count++;
          }
@@ -45,9 +45,6 @@ public:
          cout << "Error: no such student exists.\n";
       }
    }
-
-  
-
 };
 
 class Student : public IMG_Member
@@ -57,11 +54,12 @@ class Student : public IMG_Member
 public:
    Student(string name) : IMG_Member(name, "Student") {}
 
-   virtual int give_choices() const override{
-            cout << "Choose an action:" << endl;
-cout << "1. Submit assignment" << endl;
-cout << "2. View assignment status" << endl;
-cout << "3. Submit for recheck" << endl;
+   virtual int give_choices() const override
+   {
+      cout << "Choose an action:" << endl;
+      cout << "1. Submit assignment" << endl;
+      cout << "2. View assignment status" << endl;
+      cout << "3. Submit for recheck" << endl;
 
       int choice;
       cin >> choice;
@@ -69,190 +67,213 @@ cout << "3. Submit for recheck" << endl;
       return choice;
    }
 
+   void submit_for_recheck(string s_name, string date)
+   {
+      string line;
+      string filename = s_name + ".txt";
+      ifstream file(filename);
+      ofstream tempFile("temp.txt");
 
-   
+      if (file.is_open() && tempFile.is_open())
+      {
+         string assignment;
+         cout << "Enter the name of the assignment: ";
+         cin.ignore();
+         getline(cin, assignment);
+         int recheck=0;
+         int r=0;
+         bool assignmentFound = false;
+         while (getline(file, line))
+         {
+               if(recheck){
+                   tempFile << "Suggested iteration done "
+                        << " on " << date << endl;
+              
+               recheck=0;
+               r=1;
+               }
 
-void submit_for_recheck(string s_name, string date){
-    string line;
-    string filename = s_name + ".txt";
-    ifstream file(filename);
-    ofstream tempFile("temp.txt");
-
-    if (file.is_open() && tempFile.is_open())
-    {
-        string assignment;
-        cout << "Enter the name of the assignment: ";
-        cin.ignore();
-        getline(cin, assignment);
-
-      
-
-        bool assignmentFound = false;
-        while (getline(file, line))
-        {
             tempFile << line << endl;
             if (line.find(assignment) != string::npos)
-            {
-                tempFile << "Suggested iteration done " <<" on "<< date << endl;
-                assignmentFound = true;
+            {     if(line.find("Review")!=string::npos){
+               recheck=1;
+
             }
-        }
+               assignmentFound = true;
+            }
+         }
 
-        file.close();
-        tempFile.close();
+         file.close();
+         tempFile.close();
 
-        if (assignmentFound)
-        {
+         if (assignmentFound)
+         {
             remove(filename.c_str());
             rename("temp.txt", filename.c_str());
-            cout << "Submitted\n";
-        }
-        else
-        {
+            if(r==1){ 
+               cout << "Submitted\n";
+            }else{
+               cout<<"Assignment not reviewed\n";
+            }
+            
+         }
+         else
+         {
             remove("temp.txt");
             cout << "Assignment not found\n";
-        }
-    }
-    else
-    {
-        cout << "Error: Unable to open file\n";
-    }
-}
-  
+         }
+      }
+      else
+      {
+         cout << "Error: Unable to open file\n";
+      }
+   }
+
    void submit_assignment(string s_name, string date)
    {
 
+      string line;
+      string filename = s_name + ".txt";
+      ifstream file(filename);
+      ofstream tempFile("temp.txt");
 
-       string line;
-    string filename = s_name + ".txt";
-    ifstream file(filename);
-    ofstream tempFile("temp.txt");
+      if (file.is_open() && tempFile.is_open())
+      {
+         string assignment;
+         cout << "Enter the name of the assignment: ";
+         cin.ignore();
+         getline(cin, assignment);
 
-    if (file.is_open() && tempFile.is_open())
-    {
-        string assignment;
-        cout << "Enter the name of the assignment: ";
-        cin.ignore();
-        getline(cin, assignment);
-
-      
-
-        bool assignmentFound = false;
-        while (getline(file, line))
-        {
+         bool assignmentFound = false;
+         while (getline(file, line))
+         {
             tempFile << line << endl;
             if (line.find(assignment) != string::npos)
             {
-                tempFile << "Assignment submitted " <<" on "<< date << endl;
-                assignmentFound = true;
+               tempFile << "Assignment submitted "
+                        << " on " << date << endl;
+               assignmentFound = true;
             }
-        }
+         }
 
-        file.close();
-        tempFile.close();
+         file.close();
+         tempFile.close();
 
-        if (assignmentFound)
-        {
+         if (assignmentFound)
+         {
             remove(filename.c_str());
             rename("temp.txt", filename.c_str());
             cout << "Submitted\n";
-        }
-        else
-        {
+         }
+         else
+         {
             remove("temp.txt");
             cout << "Assignment not found\n";
-        }
-    }
-    else
-    {
-        cout << "Error: Unable to open file\n";
-    }
+         }
+      }
+      else
+      {
+         cout << "Error: Unable to open file\n";
+      }
    }
 };
 
 class Reviewer : public IMG_Member
 {
 
-
-
-
 public:
    Reviewer(string name) : IMG_Member(name, "Reviewer") {}
 
-    virtual int give_choices() const override{
-   cout << "Choose an action:" << endl;
-cout << "1. See assignment status" << endl;
-cout << "2. Give review" << endl;
-cout << "3. Add a new student" << endl;
-cout << "4. See personal info" << endl;
-cout<<"5. Add an assignment"<<endl;
-int choice;
+   virtual int give_choices() const override
+   {
+      cout << "Choose an action:" << endl;
+      cout << "1. See assignment status" << endl;
+      cout << "2. Give review" << endl;
+      cout << "3. Add a new student" << endl;
+      cout << "4. See personal info" << endl;
+      cout << "5. Add an assignment" << endl;
+      int choice;
       cin >> choice;
 
       return choice;
- }
+   }
 
+   void give_review(string &s_name, string date) const
+   {
+      string line;
+      string filename = s_name + ".txt";
+      ifstream file(filename);
+      ofstream tempFile("temp.txt");
 
+      if (file.is_open() && tempFile.is_open())
+      {
+         string assignment;
+         cout << "Enter the name of the assignment: ";
+         cin.ignore();
+         getline(cin, assignment);
 
-  void give_review(string &s_name, string date) const
-{
-    string line;
-    string filename = s_name + ".txt";
-    ifstream file(filename);
-    ofstream tempFile("temp.txt");
+         bool assignmentFound = false;
+         int found = 0;
+         int r = 0;
+         while (getline(file, line))
+         {
 
-    if (file.is_open() && tempFile.is_open())
-    {
-        string assignment;
-        cout << "Enter the name of the assignment: ";
-        cin.ignore();
-        getline(cin, assignment);
-
-        string review;
-        cout << "Enter the review: ";
-        getline(cin, review);
-
-        bool assignmentFound = false;
-        while (getline(file, line))
-        {
+            if (found)
+            {
+               if (line.find("submitted") != string::npos)
+               {
+                  string review;
+                  cout << "Enter the review: ";
+                  cin.ignore();
+                  getline(cin, review);
+                  tempFile << "Review: " << review << " on " << date << endl;
+               }
+               else
+               {
+                  cout << "Assignment not submitted yet! You can't give review\n";
+                  r = 1;
+               }
+               found = 0;
+            }
             tempFile << line << endl;
             if (line.find(assignment) != string::npos)
             {
-                tempFile << "Review: " << review <<" on "<<date<< endl;
-                assignmentFound = true;
+               found++;
+
+               assignmentFound = true;
             }
-        }
+         }
 
-        file.close();
-        tempFile.close();
+         file.close();
+         tempFile.close();
 
-        if (assignmentFound)
-        {
+         if (assignmentFound)
+         {
             remove(filename.c_str());
             rename("temp.txt", filename.c_str());
             remove("temp.txt");
-            cout << "Review added successfully\n";
-        }
-        else
-        {
+            if (r = 0)
+            {
+               cout << "Review added successfully\n";
+            }
+         }
+         else
+         {
             remove("temp.txt");
             cout << "Assignment not found\n";
-        }
-    }
-    else
-    {
-        cout << "Error: Unable to open file\n";
-    }
-}
+         }
+      }
+      else
+      {
+         cout << "Error: Unable to open file\n";
+      }
+   }
 
-void add_assignment(const string &s_name, const string &date)
-{
+   void add_assignment(const string &s_name, const string &date)
+   {
 
-   system("./find.sh");
-   
- 
-}
-
+      system("./find.sh");
+   }
 
    void add_student(string &s_name)
    {
@@ -281,15 +302,16 @@ void add_assignment(const string &s_name, const string &date)
       file << "----------------------------\n";
       string line;
       ifstream assignment("ASSIGNMENT.txt");
-      if(file.is_open()&&assignment.is_open()){
-         while(getline(assignment,line)){
-            file<<line<<endl;
+      if (file.is_open() && assignment.is_open())
+      {
+         while (getline(assignment, line))
+         {
+            file << line << endl;
          }
       }
       assignment.close();
       file.close();
    }
-
 
    void personal_info(string &s_name)
    {
@@ -302,9 +324,12 @@ void add_assignment(const string &s_name, const string &date)
          while (getline(file, line) && count < 3)
          {
             cout << line << endl;
+            count++;
          }
-      }else{
-         cout<<"Error: no such student exists\n";
+      }
+      else
+      {
+         cout << "Error: no such student exists\n";
       }
    }
 };
@@ -315,102 +340,104 @@ int main()
    string name;
    int role;
 
-
-cout << "Choose your role:" << endl;
-cout << "1. Reviewer" << endl;
-cout << "2. Student" << endl;
+   cout << "Choose your role:" << endl;
+   cout << "1. Reviewer" << endl;
+   cout << "2. Student" << endl;
 
    cin >> role;
    cout << "Enter your name" << endl;
    cin >> name;
-   cout<<"************************************\n";
-   cout<<"Welcome "<<name<<"!\n";
+   cout << "************************************\n";
+   cout << "Welcome " << name << "!\n";
 
    if (role == 1)
    {
       Reviewer *r_one = new Reviewer(name);
       string date = r_one->get_current_date();
-     
+
       string s_name;
       char num;
-      do{ 
+      do
+      {
 
-         int choice=r_one->give_choices();
+         int choice = r_one->give_choices();
 
-    
-      if (choice == 1)
-      {   
-      cout << "Enter the name of student\n";
-      cin >> s_name;
-         r_one->view_assignment_status(s_name);
-      }
-      else if (choice == 2)
-      {
-            
-      cout << "Enter the name of student\n";
-      cin >> s_name;
-         r_one->give_review(s_name,date);
-      }
-      else if (choice == 3)
-      {
-            
-      cout << "Enter the name of student\n";
-      cin >> s_name;
-         r_one->add_student(s_name);
-      }else if(choice == 4){
-            
-      cout << "Enter the name of student\n";
-      cin >> s_name;
-         r_one->personal_info(s_name);
-      }else if(choice==5){
-         r_one->add_assignment(s_name,date);
-      }
-      else
-      {
-         cout << "OOPs!!!wrong choice\n";
-      }
-      cout<<"press 1 to exit or any other key to continue\n";
-      cin>>num;
-      }while(num!=49);
+         if (choice == 5)
+         {
+            r_one->add_assignment(s_name, date);
+         }
+         else
+         {
+
+            cout << "Enter the name of student\n";
+            cin >> s_name;
+            if (choice == 1)
+            {
+
+               r_one->view_assignment_status(s_name);
+            }
+            else if (choice == 2)
+            {
+
+               r_one->give_review(s_name, date);
+            }
+            else if (choice == 3)
+            {
+
+               r_one->add_student(s_name);
+            }
+            else if (choice == 4)
+            {
+
+               r_one->personal_info(s_name);
+            }
+
+            else
+            {
+               cout << "OOPs!!!wrong choice\n";
+            }
+         }
+
+         cout << "press 1 to exit or any other key to continue\n";
+         cin >> num;
+      } while (num != 49);
       delete r_one;
    }
    else if (role == 2)
    {
 
       Student *s_one = new Student(name);
-       char num;
-       do{ 
+      char num;
+      do
+      {
 
+         int choice = s_one->give_choices();
+         string date = s_one->get_current_date();
 
-      int choice=s_one->give_choices();
-     string date=s_one->get_current_date();
-     
-      
-      if (choice == 1)
-      {
-         s_one->submit_assignment(name, date);
-      }
-      else if (choice == 2)
-      {
-         s_one->view_assignment_status(name);
-      }
-      else if (choice == 3)
-      {
-         s_one->submit_for_recheck(name, date);
-      }
-      else
-      {
-         cout << "Sorry!!! wrong choice\n";
-      }
-      cout<<"press 1 to exit or any other key to continue\n";
-      cin>>num;
-      }while(num!=49);
+         if (choice == 1)
+         {
+            s_one->submit_assignment(name, date);
+         }
+         else if (choice == 2)
+         {
+            s_one->view_assignment_status(name);
+         }
+         else if (choice == 3)
+         {
+            s_one->submit_for_recheck(name, date);
+         }
+         else
+         {
+            cout << "Sorry!!! wrong choice\n";
+         }
+         cout << "press 1 to exit or any other key to continue\n";
+         cin >> num;
+      } while (num != 49);
       delete s_one;
    }
-   
+
    else
    {
       cout << "Invalid role\n";
    }
 }
-
